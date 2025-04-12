0e28f92aaabb08425b6b1e9d03a80ecdd613a9c1
Nick1Z2 <nick.zerreich@gmail.com>
2025-04-12 02:18:49 +0200

    Add productivity support for Scaled Steel Beam when using Space Age
    This commit adds support for Space Age by registering a productivity bonus
    for the Scaled Steel Beam recipe via the steel-plate-productivity technology.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,3 +2,11 @@ table.insert(data.raw["technology"]["steel-processing"].effects, {
   type = "unlock-recipe",
   recipe = "NM_steel-plate"
 })
+
+if mods["space-age"] then
+table.insert(data.raw["technology"]["steel-plate-productivity"].effects, {
+  type = "change-recipe-productivity",
+  recipe = "NM_steel-plate",
+  change = 0.1
+})
+end
